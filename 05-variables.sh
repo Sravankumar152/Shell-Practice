de9d4a1d331b32047b192e50ce9df517be9510a9
Sravankumar152 <thanatisravankumar756@outlook.com>
2026-02-02 00:15:55 +0530

    getting output from the command using variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
 
-Starttime=$(date+%s)
+Starttime=$(date +%s)
 
 echo "Script started at:$Starttime"
 
 sleep 20
 
-Endtime=$(date+%s)
+Endtime=$(date +%s)
 
 echo "Script ended at:$Endtime"
 
