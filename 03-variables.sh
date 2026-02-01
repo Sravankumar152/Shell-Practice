d4b27aacdacb72410615bb5f234c2d1634bcc043
Sravankumar152 <thanatisravankumar756@outlook.com>
2026-02-01 23:32:33 +0530

    adding variables.sh

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PER1=SRAVAN
+PER2=sivaram
+
+echo "$PER1:: Hi $PER2, How are you?"
+echo "$PER2:: Hi $PER1, I am fine thanks for asking. How are you doing?"
+echo "$PER1:: doing fine, What are you learning $PER2?"
+echo "$PER2:: I am learn shell scripting"
