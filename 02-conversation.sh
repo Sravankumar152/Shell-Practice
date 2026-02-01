049de830d6d191c99e7ba0bc56fb9f94d11de094
Sravankumar152 <thanatisravankumar756@outlook.com>
2026-02-01 23:24:06 +0530

    conversation.sh

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Sravan:: Hi Sivaram, How are you?"
+echo "Sivaram:: Hi Sravan, I am fine thanks for asking. How are you doing?"
+echo "Sravan:: doing fine, What are you learning sivaram?"
+echo "Sivaram:: I am learn shell scripting"
