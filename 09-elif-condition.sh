75b16fa50875defc891b6542ce4a987058ec87ea
Sravankumar152 <thanatisravankumar756@outlook.com>
2026-02-04 11:46:48 +0530

    elif conditions

---
+++ 09-elif-condition.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NUM=50
+
+if [ $NUM -gt 50 ]; then
+
+    echo "The number: $NUM is greater than 50"
+elif [ $NUM -eq 50 ]; then
+
+    echo "The Number: $NUM is equal to 50"
+
+else
+    echo "The Number: $NUM is less than 50"
+
+fi
