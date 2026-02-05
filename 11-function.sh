5394df18284cf6ba082a47b156315a474f98dd2a
Sravankumar152 <thanatisravankumar756@outlook.com>
2026-02-06 00:08:10 +0530

    functions.sh

---
+++ 11-function.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+USER_ID=0
+
+if [ $USER_ID -ne 0 ]; then
+    echo "Please run this as a root user"
+    exit 1
+fi
+
+validate(){
+    if [$1 -ne 0 ]; then
+        echo "$2 .... failure"
+        exit 1
+    else
+        echo "$2 ... success"
+    fi
+}
+
+dnf install nginx -y
+validate $? "Installing nginx"
+
+dnf install mysql -y
+validate $? "Installing mysql"
