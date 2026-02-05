828c734f69a5563296546bc5937b4b045113de4a
Sravankumar152 <thanatisravankumar756@outlook.com>
2026-02-06 01:37:07 +0530

    pushing for loop

---
+++ 13-loops.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+LOGS_FOLDER="/var/log/shell-logs"
+LOGS_FILE="/var/log/shell-logs/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access"
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2 ... FAILURE" | tee -a &>> $LOGS_FILE
+        exit 1
+    else
+        echo "$2 ... SUCCESS" | tee -a &>> $LOGS_FILE
+    fi
+}
+
+for package in $@
+do
+    dnf install $package -y | tee -a &>> $LOGS_FILE
+    VALIDATE $? "$package installation"
+done
